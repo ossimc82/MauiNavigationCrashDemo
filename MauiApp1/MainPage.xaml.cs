0a940bd15c7e6b15c0d1ca7511770dec4335302b
ossimc82 <speisewagen96@gmx.de>
2021-11-22 17:38:30 +0100

    Initial Commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Page1 _page1;
+
+        public MainPage(Page1 page1)
+        {
+            _page1 = page1;
+
+            InitializeComponent();
+        }
+
+        private void OnPage1Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(_page1);
+        }
+    }
+}
---
+++ MauiApp1/Page1.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace MauiApp1
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
